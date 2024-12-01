b3c3bf5df2076c71b3f37983031355b294973c6b
george_nithis <georgekishore743@gmail.com>
2024-12-01 19:44:47 +0530

    added stage layer, basic cleaning done

---
+++ models/stage/stage_cleaned_customer_data.sql
@@ -0,0 +1,20 @@
+with cleaned_customer_data as(
+    select 
+    *
+    from
+        E_COMMERCE_UK.DEV.raw_customer_data
+)
+    select
+        INVOICE_NUMBER,
+        STOCK_CODE,
+        PRODUCT_DESCRIPTION,
+        ORDER_QUANTITY,
+        TO_TIMESTAMP(INVOICE_DATE || ':00', 'MM/DD/YYYY HH24:MI:SS') AS InvoiceDateCleaned,
+        UNIT_PRICE,
+        CUSTOMER_ID,
+        COUNTRY,
+        (ORDER_QUANTITY * UNIT_PRICE) as revenue
+    from    
+        cleaned_customer_data
+    where 
+        INVOICE_NUMBER IS NOT NULL
